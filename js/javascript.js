9925f15d4a1cac9b940ad91f6b0985399e2f26df
glasspinnA <ozze-1995@hotmail.com>
2017-03-07 15:59:43 +0100

    Fixade getLocation på kartan

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,36 @@
+var map;
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 55.5916628, lng: 12.9875187},
-          zoom: 13
-        });
-        var infoWindow = new google.maps.InfoWindow({map: map});
+	map = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: 55.5916628, lng: 12.9875187},
+	  zoom: 13
+	});
+}
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
+$( "#searchBtn" ).click(function() {
+	alert("Test");
+	var userPos;
+    var infoWindow = new google.maps.InfoWindow({map: map});
+	
+	if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
+			userPos = {lat:pos["lat"], lng:pos["lng"]};
             console.log("LAT: " + pos["lat"])
             console.log("LNG: " + pos["lng"])
+			
+			var request = {
+				location: userPos,
+				radius: 500,
+				type: ['restaurant']	 
+			}
+			
+			infowindow = new google.maps.InfoWindow();
+			var service = new google.maps.places.PlacesService(map);
+			//service.textSearch(request, callback);
+			service.nearbySearch(request, callback);
 
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
@@ -25,7 +42,39 @@ function initMap() {
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
-      }
+	});
+
+/*
+* Funktion som tar emot alla resturanger i närheten från API
+*/
+function callback(results, status) {
+	if (status === google.maps.places.PlacesServiceStatus.OK) {
+	  for (var i = 0; i < results.length; i++) {
+		createMarker(results[i]);
+	  }
+	}
+}
+
+/**
+* Funktion som skapar en marker och sätter ut den på kartan
+*/
+function createMarker(place) {
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({
+	  map: map,
+	  position: place.geometry.location
+	});
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+		'Place ID: ' + place.place_id + '<br>' +
+		place.formatted_address + '</div>');
+		infowindow.open(map, this);
+	});
+}
+     
+       
+      
 
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
