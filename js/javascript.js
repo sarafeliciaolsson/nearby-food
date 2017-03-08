573717dccff5ae779475c08a69c1740be8c214db
glasspinnA <ozze-1995@hotmail.com>
2017-03-08 10:48:55 +0100

    geolocation

--- js/javascript.js
+++ js/javascript.js
@@ -1,25 +1,23 @@
-var map;
+//var map;
 var userPosition;
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-	  center: {lat: 55.5916628, lng: 12.9875187},
-	  zoom: 12
-	});
-}
+ function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
 
-$( ".ourBtn" ).click(function() {
-	alert("Test");
-	
-	if (navigator.geolocation) {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-			userPosition = {lat:pos["lat"], lng:pos["lng"]};
-            console.log("LAT: " + pos["lat"])
-            console.log("LNG: " + pos["lng"])
-			console.log(userPosition);
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
           });
@@ -27,71 +25,14 @@ $( ".ourBtn" ).click(function() {
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
-	});
+      }
 
-$( "#searchBtn" ).click(function() {
-	if(userPosition != null){
-		var infoWindow = new google.maps.InfoWindow({map: map});
-		var request = {
-			location: userPosition,
-			radius: 500,
-			type: ['restaurant']	 
-		}
-
-		infowindow = new google.maps.InfoWindow();
-		var service = new google.maps.places.PlacesService(map);
-		service.textSearch(request, callback);
-		//service.nearbySearch(request, callback);
-
-		infoWindow.setPosition(userPosition);
-		infoWindow.setContent('Location found.');
-		map.setCenter(userPosition);	
-	}else{
-		alert("Användarens position finns inte");
-	}
-	
-});
-
-/*
-* Funktion som tar emot alla resturanger i närheten från API
-*/
-function callback(results, status) {
-	if (status === google.maps.places.PlacesServiceStatus.OK) {
-	  for (var i = 0; i < results.length; i++) {
-		createMarker(results[i]);
-	  }
-	}
-}
-
-/**
-* Funktion som skapar en marker och sätter ut den på kartan
-*/
-function createMarker(place) {
-	var placeLoc = place.geometry.location;
-	var marker = new google.maps.Marker({
-	  map: map,
-	  position: userPosition
-	});
-	
-	var marker1 = new google.maps.Marker({
-	  map: map,
-	  position: place.geometry.location
-	});
-
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-		'Place ID: ' + place.place_id + '<br>' +
-		place.formatted_address + '</div>');
-		infowindow.open(map, this);
-	});
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-	infoWindow.setPosition(pos);
-	infoWindow.setContent(browserHasGeolocation ?
-						  'Error: The Geolocation service failed.' :
-						  'Error: Your browser doesn\'t support geolocation.');
-}
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
 
 
 // ----- DÖLJER ALLT UTOM FÖRSTASIDAN I FÖRSTA LÄGET & VISAR ALLT NÄR MAN TRYCKER PÅ SÖK -----
