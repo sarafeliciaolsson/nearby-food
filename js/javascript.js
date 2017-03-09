97f92278e0705126085b43c5d09dc8b2844233b5
glasspinnA <ozze-1995@hotmail.com>
2017-03-09 11:37:26 +0100

    test geolocation

--- js/javascript.js
+++ js/javascript.js
@@ -1,38 +1,100 @@
-//var map;
+var map;
 var userPosition;
- function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 6
-        });
-        var infoWindow = new google.maps.InfoWindow({map: map});
+var infowindow;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: 55.5916628, lng: 12.9875187},
+	  zoom: 12
+	});
+	
+	infowindow = new google.maps.InfoWindow();
+	if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        //x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+function showPosition(position) {
+    var latlon = {lat:position.coords.latitude, lng: position.coords.longitude};
+	console.log(latlon);
+	userPosition = latlon;
+	makeSearch(userPosition);
+}
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
+function makeSearch(userPosition){
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: userPosition,
+          radius: 500,
+          type: ['store']
+        }, callback);
+	console.log("HEJ");
+}
+
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
         }
       }
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
       }
+
+
+
+/*
+		
+	var request = {
+		location: userPosition,
+		radius: 500,
+		type: ['restaurant']	 
+	}
+	
+			infowindow = new google.maps.InfoWindow();
+		var service = new google.maps.places.PlacesService(map);
+		service.textSearch(request, callback);
+		//service.nearbySearch(request, callback);
+
+		infoWindow.setPosition(userPosition);
+		infoWindow.setContent('Location found.');
+		map.setCenter(userPosition);
+}
+
+function callback(results, status) {
+	if (status === google.maps.places.PlacesServiceStatus.OK) {
+	  for (var i = 0; i < results.length; i++) {
+		createMarker(results[i]);
+	  }
+	}
+}
+
+function createMarker(place) {
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({
+	  map: map,
+	  position: place.geometry.location
+});
+
+google.maps.event.addListener(marker, 'click', function() {
+  infowindow.setContent(place.name);
+  infowindow.open(map, this);
+});
+}
+	*/
 
 
 // ----- DÖLJER ALLT UTOM FÖRSTASIDAN I FÖRSTA LÄGET & VISAR ALLT NÄR MAN TRYCKER PÅ SÖK -----
