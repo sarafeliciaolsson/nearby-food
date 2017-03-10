0a3112aef6278c7bd395600f7381e87c0cfceda4
glasspinnA <ozze-1995@hotmail.com>
2017-03-10 15:55:04 +0100

    Fixade listan och allt det andra

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,47 @@
 var map;
-var userPosition;
 var infowindow;
+var service;
+var userPosition;
+var idCounter = 0;
+var listArray = new Array();
 
 /*
 * Funktion som initiera Google Maps kartan och tar redan
-* användarens position med geolocation
 */
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: 55.5916628, lng: 12.9875187},
 	  zoom: 12
 	});
+	service = new google.maps.places.PlacesService(map);
+	infowindow = new google.maps.InfoWindow();
 }
 
+/*
+* Funktion som triggras när man klickar på Get Location knappen
+* funktionen tar reda på användarens position
+*/
 $("#getLocationBtn").click(function() {
+	var location_timeout = setTimeout("geolocFail()", 10000);
+	
 	if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(function(position){
+			clearTimeout(location_timeout);
+			userPosition = {lat:position.coords.latitude, lng: position.coords.longitude};
+			console.log(userPosition);
+		},function(error) {
+        	clearTimeout(location_timeout);
+        	geolocFail();
+		});
 	} else {
-        //x.innerHTML = "Geolocation is not supported by this browser.";
+        alert("Geolocation is not supported by this browser.");
     }
 });
 
+
+/*
+* Funktion som triggas när man klickar på sök knappen
+*/
 $("#searchBtn").click(function() {
 	if(userPosition != null){
 		makeSearch(userPosition);	
@@ -35,28 +56,18 @@ $("#searchBtn").click(function() {
 });
 
 
-/*
-* Funktion som tar emot användarens position och sätter ut marker
-* för användarens position
-*/
-function showPosition(position) {
-    var latlon = {lat:position.coords.latitude, lng: position.coords.longitude};
-	console.log(latlon);
-	userPosition = latlon;
-}
 
 /*
-* Funktion som gör sökningen av närliggande resturanger från användarens * position
+* Funktion som gör sökningen av närliggande resturanger från användarens position
 */
 function makeSearch(userPosition){
-	var t = userPosition
-	var service = new google.maps.places.PlacesService(map);
-
-	service.nearbySearch ({
-	  location: t,
-	  radius: 500,
-	  type: ['restaurant']
-	}, callback);
+	var userPos = userPosition;
+	var request = {
+		location: userPos,
+	  	radius: 500,
+	  	type: ['restaurant']
+	}
+	service.nearbySearch(request, callback);
 }
 
 /*
@@ -65,39 +76,74 @@ function makeSearch(userPosition){
 function callback(results, status) {
 	if (status === google.maps.places.PlacesServiceStatus.OK) {
 		for (var i = 0; i < results.length; i++) {
-			createMarker(results[i]);
-	  	}
+			//createMarker(results[i]);
+			$("#resultFromAPI").append('<h1 class="selectedRestaurang" id='+idCounter+'>' + results[i].name + '</h1>');
+			listArray.push(results[i]);
+			idCounter++;
+		}
+		
+		createUserMarker();
 	}
 }
 
 /*
-* Funktion som sätter ut alla resturanger i kartan och sätter ut en 
-* ikon för varje resturang
+* Funktion som skapar markern för användarens position i kartan
 */
-
-function createMarker(place) {
-	var placeLoc = place.geometry.location;
-	
-	var marker1 = new google.maps.Marker({
+function createUserMarker(){
+	var userMarker = new google.maps.Marker({
 		position: userPosition,
 		map: map,
-		title: 'Hello World!',
+		title: 'Din position',
 		icon: 'http://maps.google.com/mapfiles/ms/icons/blue.png'	  
 	});
 	
-	
-	
+	google.maps.event.addListener(userMarker, 'click', function() {
+		infowindow.setContent("Din position");
+		infowindow.open(map, this);
+	});
+}
+
+/*
+* Funktion som triggas när man klickar på ett resturangnamn i listan
+* funktionen tar reda på vilken resturang man har klickat
+*/
+$('#resultFromAPI').on('click', '.selectedRestaurang', function(){
+	console.log(this.id);
+	console.log(listArray[this.id]);
+	var currentId = listArray[this.id];
+	createMarker(currentId);
+});
+
+/*
+* Funktion som sätter ut en marker för en vald resturang
+* Visar även detljer för varje resturang i liten ruta när 
+* man klickar på en ikon
+*/
+
+function createMarker(place) {
 	var marker = new google.maps.Marker({
 	  map: map,
 	  position: place.geometry.location
 	});
+	
+	marker.addListener('click', function(){
+		var request = {
+			reference: place.reference
+		};
+		service.getDetails(request, function(details, status) {
 
+		  infowindow.setContent([
+			details.name,
+			details.formatted_address,
+			details.website,
+			details.rating,
+			details.formatted_phone_number].join("<br />"));
+		  infowindow.open(map, marker);
+		});
+	})
+	
 	map.setZoom(15);
 	map.panTo(marker.position);
-	google.maps.event.addListener(marker, 'click', function() {
-	  infowindow.setContent(place.name);
-	  infowindow.open(map, this);
-	});
 }
 
 // ----- DÖLJER ALLT UTOM FÖRSTASIDAN I FÖRSTA LÄGET & VISAR ALLT NÄR MAN TRYCKER PÅ SÖK -----
